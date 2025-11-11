e43de5db91cae9bfe37410c5d9b7ff9150a591b3
Celixx <alonsomurua@gmail.com>
2025-11-11 17:20:27 -0300

    Mantenedor Socio final

--- Seguros_Broker/Seguros_Broker/Repositorio/SocioRep.cs
+++ Seguros_Broker/Seguros_Broker/Repositorio/SocioRep.cs
@@ -1,16 +1,65 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using Seguros_Broker.Modelo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Data.SqlClient;
-using Seguros_Broker.Modelo;
+using System.Windows;
 
 namespace Seguros_Broker.Repositorio
 {
     internal class SocioRep : Repositorio
     {
-      public List<Socio> GetSocios() 
+        public Socio? GetSocio(int ID)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = "SELECT * FROM SOCIO WHERE ID=@ID";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+
+                        command.Parameters.AddWithValue("@ID", ID);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                Socio socio = new Socio();
+
+                                socio.tipoId = reader.IsDBNull(0) ? null : reader.GetString(0);
+                                socio.ID = reader.IsDBNull(1) ? 0 : reader.GetInt32(1);
+                                socio.nombre = reader.IsDBNull(2) ? null : reader.GetString(2);
+                                socio.aPaterno = reader.IsDBNull(3) ? null : reader.GetString(3);
+                                socio.aMaterno = reader.IsDBNull(4) ? null : reader.GetString(4);
+                                socio.fono = reader.IsDBNull(5) ? 0 : reader.GetInt32(5);
+                                socio.celular = reader.IsDBNull(6) ? 0 : reader.GetInt32(6);
+                                socio.mail = reader.IsDBNull(7) ? null : reader.GetString(7);
+                                socio.fax = reader.IsDBNull(8) ? 0 : reader.GetInt32(8);
+                                socio.direccion = reader.IsDBNull(9) ? null : reader.GetString(9);
+                                socio.observacion = reader.IsDBNull(10) ? null : reader.GetString(10);
+                                socio.comision = reader.IsDBNull(11) ? 0 : reader.GetInt32(11);
+                                socio.porcentajeComision = reader.IsDBNull(12) ? 0 : reader.GetInt32(12);
+
+                                return socio;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Se encontró el ID, pero ocurrió un error al leer los datos:\n\n" + ex.Message, "Error de Lectura", MessageBoxButton.OK, MessageBoxImage.Error);
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
+
+            return null;
+        }
+
+        public List<Socio> GetSocios()
         {
             var socios = new List<Socio>();
 
@@ -56,6 +105,121 @@ namespace Seguros_Broker.Repositorio
             }
 
             return socios;
+        }
+
+        public async Task<(bool success, string? errorMessage)> CreateSocioAsync(Socio socio)
+        {
+            if (socio == null)
+                return (false, "Socio nulo.");
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    await connection.OpenAsync();
+
+                    // Verificar existencia por codigo o ID
+                    const string checkSql = "SELECT COUNT(1) FROM SOCIO WHERE ID = @ID";
+                    using (var checkCmd = new SqlCommand(checkSql, connection))
+                    {
+                        checkCmd.Parameters.Add("@ID", System.Data.SqlDbType.Int).Value = socio.ID;
+
+                        var exists = (int)await checkCmd.ExecuteScalarAsync();
+                        if (exists > 0)
+                        {
+                            return (false, "Ya existe un Socio con el mismo Identificación.");
+                        }
+                    }
+
+                    using (var tran = connection.BeginTransaction())
+                    {
+                        const string insertSql = @"INSERT INTO SOCIO
+                                                                    (TipoID, ID, Nombre, APaterno, AMaterno, Fono, Celular, Mail, Fax, Direccion, Observacion, Comision, PorcentajeComision)
+                                                               VALUES (@TipoID, @ID, @Nombre, @APaterno, @AMaterno, @Fono, @Celular, @Mail, @Fax, @Direccion, @Observacion, @Comision, @PorcentajeComision);";
+
+                        using (var cmd = new SqlCommand(insertSql, connection, tran))
+                        {
+                            // Parámetros con tipos 
+                            cmd.Parameters.Add("@TipoID", System.Data.SqlDbType.NVarChar, 15).Value = (object)socio.tipoId ?? DBNull.Value;
+                            cmd.Parameters.Add("@ID", System.Data.SqlDbType.Int).Value = socio.ID;
+                            cmd.Parameters.Add("@Nombre", System.Data.SqlDbType.NVarChar, 25).Value = (object)socio.nombre?? DBNull.Value;
+                            cmd.Parameters.Add("@APaterno", System.Data.SqlDbType.NVarChar, 25).Value = (object)socio.aPaterno ?? DBNull.Value;
+                            cmd.Parameters.Add("@AMaterno", System.Data.SqlDbType.NVarChar, 25).Value = (object)socio.aMaterno ?? DBNull.Value;
+                            cmd.Parameters.Add("@Fono", System.Data.SqlDbType.Int).Value = socio.fono;
+                            cmd.Parameters.Add("@Celular", System.Data.SqlDbType.Int).Value = socio.celular;
+                            cmd.Parameters.Add("@Mail", System.Data.SqlDbType.NVarChar, 50).Value = (object)socio.mail ?? DBNull.Value;
+                            cmd.Parameters.Add("@Fax", System.Data.SqlDbType.Int).Value = socio.fax;
+                            cmd.Parameters.Add("@Direccion", System.Data.SqlDbType.NVarChar, 50).Value = (object)socio.direccion ?? DBNull.Value;
+                            cmd.Parameters.Add("@Observacion", System.Data.SqlDbType.NVarChar, 50).Value = (object)socio.observacion ?? DBNull.Value;
+                            cmd.Parameters.Add("@Comision", System.Data.SqlDbType.Int).Value = socio.comision;
+                            cmd.Parameters.Add("@PorcentajeComision", System.Data.SqlDbType.Int).Value = socio.porcentajeComision;
+
+                            int rows = await cmd.ExecuteNonQueryAsync();
+                            if (rows <= 0)
+                            {
+                                await tran.RollbackAsync();
+                                return (false, "No se insertó el registro (0 filas afectadas).");
+                            }
+
+                            await tran.CommitAsync();
+                            return (true, null);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return (false, ex.Message);
+            }
+        }
+
+        public async Task<(bool success, string? errorMessage)> UpdateSocioAsync(Socio socio)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    await connection.OpenAsync();
+                    using (var tran = connection.BeginTransaction())
+                    {
+                        const string insertSql = @"UPDATE SOCIO
+                                                    SET TipoID=@TipoID, Nombre=@Nombre, APaterno=@APaterno, AMaterno=@AMaterno, Fono=@Fono, Celular=@Celular, Mail=@Mail, Fax=@Fax, Direccion=@Direccion, Observacion=@Observacion, Comision=@Comision, PorcentajeComision=@PorcentajeComision
+                                                    WHERE ID=@ID;";
+                        using (var cmd = new SqlCommand(insertSql, connection, tran))
+                        {
+                            cmd.Parameters.Add("@TipoID", System.Data.SqlDbType.NVarChar, 15).Value = (object)socio.tipoId ?? DBNull.Value;
+                            cmd.Parameters.Add("@Nombre", System.Data.SqlDbType.NVarChar, 25).Value = (object)socio.nombre ?? DBNull.Value;
+                            cmd.Parameters.Add("@APaterno", System.Data.SqlDbType.NVarChar, 25).Value = (object)socio.aPaterno ?? DBNull.Value;
+                            cmd.Parameters.Add("@AMaterno", System.Data.SqlDbType.NVarChar, 25).Value = (object)socio.aMaterno ?? DBNull.Value;
+                            cmd.Parameters.Add("@Fono", System.Data.SqlDbType.Int).Value = socio.fono;
+                            cmd.Parameters.Add("@Celular", System.Data.SqlDbType.Int).Value = socio.celular;
+                            cmd.Parameters.Add("@Mail", System.Data.SqlDbType.NVarChar, 50).Value = (object)socio.mail ?? DBNull.Value;
+                            cmd.Parameters.Add("@Fax", System.Data.SqlDbType.Int).Value = socio.fax;
+                            cmd.Parameters.Add("@Direccion", System.Data.SqlDbType.NVarChar, 50).Value = (object)socio.direccion ?? DBNull.Value;
+                            cmd.Parameters.Add("@Observacion", System.Data.SqlDbType.NVarChar, 50).Value = (object)socio.observacion ?? DBNull.Value;
+                            cmd.Parameters.Add("@Comision", System.Data.SqlDbType.Int).Value = socio.comision;
+                            cmd.Parameters.Add("@PorcentajeComision", System.Data.SqlDbType.Int).Value = socio.porcentajeComision;
+                            cmd.Parameters.Add("@ID", System.Data.SqlDbType.Int).Value = socio.ID;
+
+                            int rows = await cmd.ExecuteNonQueryAsync();
+
+                            if (rows <= 0)
+                            {
+                                await tran.RollbackAsync();
+                                return (false, "No se actualizó el registro (0 filas afectadas).");
+                            }
+
+                            await tran.CommitAsync();
+                            return (true, null);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return (false, ex.Message);
+            }
         }
     }
 }
--- Seguros_Broker/Seguros_Broker/SocioMantenedor.xaml.cs
+++ Seguros_Broker/Seguros_Broker/SocioMantenedor.xaml.cs
@@ -1,8 +1,10 @@
-﻿using Seguros_Broker.Repositorio;
+﻿using Seguros_Broker.Modelo;
+using Seguros_Broker.Repositorio;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -30,7 +32,63 @@ namespace Seguros_Broker
 
         private void btnBuscar_Click(object sender, RoutedEventArgs e)
         {
+            int idBuscado = int.Parse(txtSearch.Text);
 
+            if (idBuscado == null)
+            {
+                MessageBox.Show("Por favor, ingrese un ID para buscar.", "Entrada Requerida", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            Socio? socioEncontrado = socioRep.GetSocio(idBuscado);
+
+            if (socioEncontrado != null)
+            {
+
+                CargarDatosSocioEnFormulario(socioEncontrado);
+
+                MessageBox.Show($"Ejecutivo encontrado: {socioEncontrado.nombre} {socioEncontrado.aPaterno}", "Búsqueda Exitosa", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show($"No se encontró ningún ejecutivo con el ID: {idBuscado}", "No Encontrado", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void CargarDatosSocioEnFormulario(Socio socio)
+        {
+
+            if (socio == null) return;
+
+            cbTipoIdentificacion.Text = socio.tipoId;
+            txtIdentificacion.Text = socio.ID.ToString();
+            txtNombre.Text = socio.nombre;
+            txtApaterno.Text = socio.aPaterno;
+            txtAmaterno.Text = socio.aMaterno;
+            txtFono.Text = socio.fono.ToString();
+            txtCelular.Text = socio.celular.ToString();
+            txtMail.Text = socio.mail;
+            txtFax.Text = socio.fax.ToString();
+            txtDireccion.Text = socio.direccion;
+            txtObservacion.Text = socio.observacion;
+            cbNivelComision.Text = GetTextoComision(socio.comision);
+            txtPorcentajeComision.Text = socio.porcentajeComision.ToString();
+        }
+
+        private string GetTextoComision(int comisionValor)
+        {
+            switch (comisionValor)
+            {
+                case 5:
+                    return "5(NIVEL POR DEFECTO)";
+                case 10:
+                    return "10";
+                case 15:
+                    return "15";
+                default:
+
+                    return "5(NIVEL POR DEFECTO)";
+            }
         }
 
         private void btnLimpiar_Click(object sender, RoutedEventArgs e)
@@ -38,9 +96,89 @@ namespace Seguros_Broker
             LimpiarFormulario();
         }
 
-        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        private async void btnGuardar_Click(object sender, RoutedEventArgs e)
         {
+            var errores = new System.Collections.Generic.List<string>();
 
+            int codigoParsed = 0;
+
+            if (cbTipoIdentificacion.SelectedItem == null || ((ComboBoxItem)cbTipoIdentificacion.SelectedItem).Content.ToString().ToUpper().Contains("SELECCIONE"))
+                errores.Add("Tipo identificación (obligatorio).");
+
+            if (string.IsNullOrWhiteSpace(txtIdentificacion.Text))
+                errores.Add("Identificación (obligatorio).");
+
+
+            if (string.IsNullOrWhiteSpace(txtNombre.Text))
+                errores.Add("Nombre (obligatorio).");
+
+            else if (txtNombre.Text.Any(char.IsDigit))
+            {
+                errores.Add("El nombre no puede contener números.");
+            }
+
+            if (!string.IsNullOrWhiteSpace(txtMail.Text) && !IsValidEmail(txtMail.Text))
+                errores.Add("Mail inválido.");
+
+            if (errores.Any())
+            {
+                MessageBox.Show("Corrija los siguientes errores:\n- " + string.Join("\n- ", errores), "Errores de validación", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            var nuevoSocio = new Socio()
+            {
+                tipoId = ((ComboBoxItem)cbTipoIdentificacion.SelectedItem).Content.ToString(),
+                ID = int.TryParse(txtIdentificacion.Text, out int f) ? f : 0,
+                nombre = txtNombre.Text.Trim(),
+                aPaterno = txtApaterno.Text.Trim(),
+                aMaterno = txtAmaterno.Text.Trim(),
+                fono = int.TryParse(txtFono.Text, out int v) ? v : 0,
+                celular = int.TryParse(txtCelular.Text, out int b) ? b : 0,
+                mail = txtMail.Text.Trim(),
+                fax = int.TryParse(txtFax.Text,out int m) ? m : 0,
+                direccion = txtDireccion.Text.Trim(),
+                observacion = txtObservacion.Text.Trim(),
+                comision = ParseNivelComision(((ComboBoxItem)cbNivelComision.SelectedItem).Content.ToString()),
+                porcentajeComision = int.TryParse(txtPorcentajeComision.Text, out int h) ? h : 0,
+            };
+
+            var result = await socioRep.CreateSocioAsync(nuevoSocio);
+
+            if (result.success)
+            {
+                MessageBox.Show("Socio guardado correctamente.", "Éxito", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                // Refrescar la grilla
+                ReadSocio();
+
+                // Limpiar formulario
+                LimpiarFormulario();
+            }
+            else
+            {
+                // Msj de error proveniente del repositorio
+                MessageBox.Show("No se pudo guardar: " + (result.errorMessage ?? "Error desconocido"), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private static int ParseNivelComision(string contenidoCombo)
+        {
+            if (string.IsNullOrWhiteSpace(contenidoCombo)) return 0;
+            var m = Regex.Match(contenidoCombo, @"\d+");
+            if (m.Success && int.TryParse(m.Value, out int v)) return v;
+            return 0;
+        }
+
+        private static bool IsValidEmail(string email)
+        {
+            try
+            {
+                return Regex.IsMatch(email,
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase);
+            }
+            catch { return false; }
         }
 
         private void btnCancelar_Click(object sender, RoutedEventArgs e)
@@ -53,14 +191,71 @@ namespace Seguros_Broker
             }
         }
 
-        private void btnActualizar_Click(object sender, RoutedEventArgs e)
+        private async void btnActualizar_Click(object sender, RoutedEventArgs e)
         {
+            var errores = new System.Collections.Generic.List<string>();
 
+            if (cbTipoIdentificacion.SelectedItem == null ||
+                ((ComboBoxItem)cbTipoIdentificacion.SelectedItem).Content.ToString().ToUpper().Contains("SELECCIONE"))
+                errores.Add("Tipo identificación (obligatorio).");
+
+            if (string.IsNullOrWhiteSpace(txtIdentificacion.Text))
+                errores.Add("Identificación (obligatorio).");
+
+            if (string.IsNullOrWhiteSpace(txtNombre.Text))
+                errores.Add("Nombre (obligatorio).");
+
+            if (errores.Any())
+            {
+                MessageBox.Show("Corrija los siguientes errores:\n- " + string.Join("\n- ", errores), "Errores de validación", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            var nuevoSocio = new Socio
+            {
+                tipoId = ((ComboBoxItem)cbTipoIdentificacion.SelectedItem).Content.ToString(),
+                ID = int.TryParse(txtIdentificacion.Text, out int f) ? f : 0,
+                nombre = txtNombre.Text.Trim(),
+                aPaterno = txtApaterno.Text.Trim(),
+                aMaterno = txtAmaterno.Text.Trim(),
+                fono = int.TryParse(txtFono.Text, out int v) ? v : 0,
+                celular = int.TryParse(txtCelular.Text, out int b) ? b : 0,
+                mail = txtMail.Text.Trim(),
+                fax = int.TryParse(txtFax.Text, out int m) ? m : 0,
+                direccion = txtDireccion.Text.Trim(),
+                observacion = txtObservacion.Text.Trim(),
+                comision = ParseNivelComision(((ComboBoxItem)cbNivelComision.SelectedItem).Content.ToString()),
+                porcentajeComision = int.TryParse(txtPorcentajeComision.Text, out int h) ? h : 0,
+            };
+
+            var result = await socioRep.UpdateSocioAsync(nuevoSocio);
+
+            if (result.success)
+            {
+                MessageBox.Show("Socio actualizada correctramente.", "Éxito", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                //Refrescar Grid
+                ReadSocio();
+
+                //Limpiar Form
+                LimpiarFormulario();
+            }
+            else
+            {
+                //Mostrar mensaje de error del repo
+                MessageBox.Show("No se pudo actualizar: " + (result.errorMessage ?? "Error desconocido"), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            var socioSeleccionado = (Socio)dataGridSocio.SelectedItem;
 
+            if (socioSeleccionado != null)
+            {
+
+                CargarDatosSocioEnFormulario(socioSeleccionado);
+            }
         }
 
         private void txtCelular_TextChanged(object sender, TextChangedEventArgs e)
@@ -89,6 +284,11 @@ namespace Seguros_Broker
             txtDireccion.Text = string.Empty;
             cbNivelComision.SelectedIndex = 0;
             txtPorcentajeComision.Text = string.Empty;
+        }
+
+        private void cbTipoIdentificacion_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
