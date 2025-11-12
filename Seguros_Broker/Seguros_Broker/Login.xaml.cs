855affa1724321a438b0d07f5b0fe008c7a7e7a1
Celixx <alonsomurua@gmail.com>
2025-11-12 10:59:56 -0300

    Login

---
+++ Seguros_Broker/Seguros_Broker/Login.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Seguros_Broker
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtUsuario_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            PlaceholderUsuario.Visibility =
+                string.IsNullOrWhiteSpace(TxtUsuario.Text) ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string usuario = TxtUsuario.Text;
+            string password = TxtPassword.Password;
+
+            if (string.IsNullOrWhiteSpace(usuario) || string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Debe ingresar usuario y contraseña.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (usuario == "admin" && password == "admin")
+            {
+
+                var ventanaPrincipal = new VentanaPrincipal();
+                ventanaPrincipal.Show();
+
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrectos.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void OlvidoContrasena_Click(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Redirigiendo al proceso de recuperación de contraseña.", "Información");
+        }
+
+        private void BtnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
